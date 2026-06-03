3d1b26e1364dfa01573b6df04fd2ffd5ef7a3e30
Vaje17 <kmjenna17@gmail.com>
2026-06-03 22:54:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// simple click effect
+document.querySelectorAll(".btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    btn.style.transform = "scale(0.95)";
+    setTimeout(() => {
+      btn.style.transform = "scale(1)";
+    }, 100);
+  });
+});
